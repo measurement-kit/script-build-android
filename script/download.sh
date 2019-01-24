c337ea79a523cb287713efce11b6131f7cbaa657
Simone Basso <bassosimone@gmail.com>
2019-01-24 17:26:39 +0100

    script: make scripts more consistent with each other

--- script/download.sh
+++ script/download.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -ex
-for dir in `ls src`; do
-  source src/$dir/download.sh
-  rm -rf pkg/$dir
-  mkdir pkg/$dir
-  curl -fsSL -o pkg/$dir/tarball.tar.gz $url
+allpkgs="libmaxminddb libressl libevent curl measurement-kit"
+for dir in $allpkgs; do
+  source ./$dir/download.sh
+  rm -rf ./pkg/$dir
+  mkdir ./pkg/$dir
+  curl -fsSL -o ./pkg/$dir/tarball.tar.gz $url
   [ "$sha256" = "`shasum -a 256 pkg/$dir/tarball.tar.gz | awk '{print $1}'`" ]
 done
