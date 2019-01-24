e117565b4e4c22e8e5b8b8ec7ae215d0dd41e0ba
Simone Basso <bassosimone@gmail.com>
2019-01-24 17:36:40 +0100

    s/build.sh: error out if NDK_ROOT is not set

--- script/build.sh
+++ script/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -e
+if [ "$NDK_ROOT" = "" ]; then
+  echo "FATAL: NDK_ROOT is not set" 1>&2
+  exit 1
+fi
 allpkgs="libmaxminddb libressl libevent curl measurement-kit"
 for pkg in $allpkgs; do
   for toolchain in `ls ./toolchain`; do
