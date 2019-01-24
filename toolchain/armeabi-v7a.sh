19999eb2623d4ab44fec5ff803ea4fc05b4a6de3
Simone Basso <bassosimone@gmail.com>
2019-01-24 17:03:01 +0100

    t/armeabi-v7a.sh: flag compatible with script-build-unix
    I've checked the flags and they seem sane.

---
+++ toolchain/armeabi-v7a.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -e
+# There is some inconsistency in tool names
+tool=arm-linux-androideabi
+compiler=armv7a-linux-androideabi16
+sysname="`uname -s|tr [A-Z] [a-z]`-`uname -m`"
+extraflags="-mfloat-abi=softfp -mfpu=vfpv3-d16 -mthumb"
+
+export AR="$tool-ar"
+export AS="$tool-as"
+export CC="${compiler}-clang"
+export CFLAGS="-O2 -fPIC $extraflags"
+export CONFIGUREFLAGS="--host=$compiler"
+export CPP="${compiler}-clang -E"
+export CXX="${compiler}-clang++"
+export CXXFLAGS="-O2 -fPIC $extraflags"
+export LD="$tool-ld"
+export MKARCH="armeabi-v7a"
+export NM="$tool-nm"
+export PATH="$NDK/toolchains/llvm/prebuilt/$sysname/bin/:$PATH"
+export RANLIB="$tool-ranlib"
+export STRIP="$tool-strip"
+
+"$@"
