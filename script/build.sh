0fdbece0fd197dd7b185a87c38b57f3ccbee696b
Simone Basso <bassosimone@gmail.com>
2019-01-24 17:29:05 +0100

    s/build.sh: make sure we actually execute commands
    Previously we're just doing a dry run.

--- script/build.sh
+++ script/build.sh
@@ -2,6 +2,6 @@
 allpkgs="libmaxminddb libressl libevent curl measurement-kit"
 for pkg in $allpkgs; do
   for toolchain in `ls ./toolchain`; do
-    echo ./toolchain/$toolchain ./$pkg/build.sh
+    ./toolchain/$toolchain ./$pkg/build.sh
   done
 done
