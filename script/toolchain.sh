8403846434965640938bffafbe590af36134164b
Simone Basso <bassosimone@gmail.com>
2019-01-24 17:20:54 +0100

    del s/toolchain.sh: this is actually unnecessary

--- script/toolchain.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-USAGE="$0 arm64|armeabi-v7a|x86|x86_64 command [args...]"
-[ "$NDK" != "" ] || { echo "FATAL: NDK variable not set"; exit 1; }
-[ "$#" -ge 2 ] || { echo "$USAGE"; exit 1; }
-toolchain=$1
-shift
-./toolchain/$toolchain.sh "$@"
