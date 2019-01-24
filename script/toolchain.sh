25bb284906374ed36861b1f9092b0605ec1ba597
Simone Basso <bassosimone@gmail.com>
2019-01-24 17:16:38 +0100

    s/toolchain.sh: better usage message

--- script/toolchain.sh
+++ script/toolchain.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
+USAGE="$0 arm64|armeabi-v7a|x86|x86_64 command [args...]"
 [ "$NDK" != "" ] || { echo "FATAL: NDK variable not set"; exit 1; }
-[ "$#" -ge 2 ] || { echo "Usage: $0 <toolchain> [command <args>...]"; exit 1; }
+[ "$#" -ge 2 ] || { echo "$USAGE"; exit 1; }
 toolchain=$1
 shift
 ./toolchain/$toolchain.sh "$@"
